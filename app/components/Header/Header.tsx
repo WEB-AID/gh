2919e8ec289efee4137b405314a40c451039bef2
WEB-AID <web.aid.dev@gmail.com>
2024-10-27 10:01:09 +0100

    first commit

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className="w-screen fixed h-24 lg:h-32 bg-white z-20 shadow-inner-orange">
+            <div className="h-full flex items-center relative md:justify-center">
+                <button
+                    className="md:hidden p-4 flex mr-auto mt-auto mb-auto"
+                >
+                    <pre>  ☰</pre>
+                </button>
+                <ul className="h-full relative hidden md:flex gap-2 lg:gap-10 justify-center items-center">
+                    <li className="border-black border-2">Пункт 1</li>
+                    <li className="border-black border-2">Пункт 2</li>
+                    <li className="border-black border-2">Пункт 3</li>
+                    <li className="ml-20 mr-20">
+                        <Image
+                            src="/logo.png"
+                            alt="logo"
+                            width="250"
+                            height="250"
+                            className="w-48 lg:w-64 absolute -translate-x-1/2 top-1/3 transform -mt-6"
+                        />
+                    </li>
+                    <li className="border-black border-2">Пункт 4</li>
+                    <li className="border-black border-2">Пункт 5</li>
+                    <li className="border-black border-2">Пункт 6</li>
+                </ul>
+                <div className="absolute right-1 top-6 md:hidden w-1/3">
+                    <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width="350"
+                        height="350"
+                    />
+                </div>
+            </div>
+        </header >
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,119 @@
 import Image from "next/image";
+import Greetings from "./components/Greetings/Greetings";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex flex-col">
+      <Header />
+      <Main />
+      <Footer />
+      <div>111111111111111111111111111111111111111111</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
     </div>
   );
 }
+
+function Main() {
+  return (
+    <main className="pt-24 lg:pt-32 flex flex-col">
+      <div>
+        <Image
+          src="/plant1.jpg"
+          alt="Plant logo"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-40vh md:h-70vh"
+        />
+      </div>
+      <Greetings />
+    </main>
+  )
+}
+
+// "use client";
+
+// import Image from "next/image";
+// import { useState } from "react";
+
+// function Header() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <header className="w-screen fixed h-32 bg-white z-20">
+//       <div className="h-full">
+//         <button
+//           className="block md:hidden p-4"
+//           onClick={(prev) => setIsOpen(!prev)}
+//         >
+//           ☰
+//         </button>
+//         <ul
+//           className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg transition-transform duration-500 ease-in-out z-30 md:static md:w-auto md:h-full md:bg-transparent md:shadow-none md:flex ${isOpen ? "translate-x-0" : "-translate-x-full"
+//             }`}
+//         >
+//           <li className="border-black border-2 p-4">Пункт 1</li>
+//           <li className="border-black border-2 p-4">Пункт 2</li>
+//           <li className="border-black border-2 p-4">Пункт 3</li>
+//           <li className="border-black border-2 p-4">Пункт 4</li>
+//           <li className="ml-20 mr-20 p-4">
+//             <Image
+//               src="/logo.png"
+//               alt="logo"
+//               width="250"
+//               height="250"
+//               sizes="25vw"
+//               className="absolute -translate-x-1/2 top-1/3 transform -mt-10"
+//               priority
+//             />
+//           </li>
+//           <li className="border-black border-2 p-4">Пункт 5</li>
+//           <li className="border-black border-2 p-4">Пункт 6</li>
+//           <li className="border-black border-2 p-4">Пункт 7</li>
+//           <li className="border-black border-2 p-4">Пункт 8</li>
+//         </ul>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default function Home() {
+//   return (
+//     <div className="flex flex-col">
+//       <Header />
+//       <main className="flex flex-col pt-32">
+//         <Image
+//           src="/plant1.jpg"
+//           alt="Plant logo"
+//           width="0"
+//           height="0"
+//           sizes="100vw"
+//           className="w-full h-auto z-11 z-10"
+//           priority
+//         />
+//         <div className="flex h-50vh overflow-hidden">
+//           <div className="w-1/3 mt-auto mb-auto ml-auto flex flex-center items-center flex-col">
+//             <h1 className="text-xl mb-5">Georgian House</h1>
+//             nam atque! Ea soluta excepturi labore, quis vero ipsum illum
+//             repellendus animi, fugit ratione tenetur fuga, consequatur
+//             blanditiis at aperiam magni dolores velit molestiae corporis maiores
+//             debitis officia? Iusto expedita tempora adipisci voluptate?
+//           </div>
+//           <Image
+//             src="/translucent-image.png"
+//             alt="Plant logo"
+//             width="0"
+//             height="0"
+//             sizes="100vw"
+//             className="w-6/12 -mt-14"
+//             priority
+//           />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
