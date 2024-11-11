839c7a5295289130121ec63c598a1c3db4d4be8b
WEB-AID <web.aid.dev@gmail.com>
2024-11-11 09:43:20 +0100

    localisation-ready commit

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import Greetings from './components/Greetings/Greetings'
+import Greetings from '../../components/Greetings/Greetings'
 
-export default function Home() {
+export default async function Home() {
     return (
         <main className="flex flex-col">
             <div className="relative">
---
+++ i18nConfig.js
@@ -0,0 +1,6 @@
+const i18nConfig = {
+    locales: ['ge', 'ru', 'en'],
+    defaultLocale: 'ge',
+}
+
+module.exports = i18nConfig
